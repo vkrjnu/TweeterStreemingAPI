dc910025f8f6d6c83e4b72ed4866e348fa243839
vkr90 <kumar.vkr90@gmail.com>
2015-12-10 03:32:17 +0530

    nltk link

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,7 +1,10 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-import json, time, math, sys, os
+import json, time, math, sys, os, re
+from nltk import pos_tag, word_tokenize
+from urllib.request import urlopen
+import urllib
 
 access_token = "388139836-nottn5MwhKBlf6cERn5R7PcRjVFfrzezGJW7h79f"
 access_token_secret = "7aPwrTvgHOyhuCxZy1HNR7LgiBjdBqtHsae1mms00eTBi"
@@ -20,14 +23,16 @@ class StdOutListner(StreamListener):
 	    self.tweets_data.append(tweet)
 	    current_time = time.localtime(time.time())
 	    time_diff = (time.mktime(current_time) - time.mktime(self.start_time)) / 60
-	    if math.floor(time_diff) == 1:
+	    if time_diff > 0:
 	        tweets_data_list.append(tuple(self.tweets_data))
 	        if self.count == 5:
 	            self.last_five = 1
+	        tweet_text = ""
 	        if self.last_five == 1:
 	        	for all_tweets in tweets_data_list :
 	        		for tweets in all_tweets :
 		        		screen_name = tweets['user']['screen_name']
+		        		tweet_text += tweets['text']
 		        		if screen_name in self.tweets_user : 
 		        			self.tweets_user[screen_name] = int(self.tweets_user[screen_name]) + 1
 		        		else:
@@ -37,13 +42,28 @@ class StdOutListner(StreamListener):
 	        	for all_tweets in tweets_data_list:
 	        		for tweets in all_tweets :
 		        		screen_name = tweets['user']['screen_name']
+		        		tweet_text += tweets['text']
+		        		#print(tweet_text)
 		        		if screen_name in self.tweets_user : 
 		        			self.tweets_user[screen_name] = int(self.tweets_user[screen_name]) + 1
 		        		else:
 		        			self.tweets_user[screen_name] = 1
 	        	self.count += 1
+	        	#for sl in link_set:
+	        		#slp = sl.encode("utf-8")
+	        		#el = urlopen(sl)
+	        		#print(el.url)
+	        	#print(link_set)
 	        self.start_time = time.localtime(time.time())
-	        print(self.tweets_user)
+	        print("****Twitter's User with total number of tweets****")
+	        for tu, tn in self.tweets_user.items():
+	        	print(tu+" : "+ str(tn))
+	        link_set = set(re.findall(r"https?://[^\s]+", tweet_text))
+	        number_of_link = len(link_set)
+	        print("****Total number of links in all tweets***")
+	        print(number_of_link)
+	        text_token = word_tokenize(tweet_text)
+	        print(pos_tag(text_token))
 	        self.tweets_user.clear()
 	        del(self.tweets_data[:])
 	    return True
@@ -51,7 +71,6 @@ class StdOutListner(StreamListener):
 	    print("Error occured" + status)
 if __name__ == "__main__":
 	try:
-		print("Execution started......")
 		stdl = StdOutListner()
 		auth = OAuthHandler(api_key, api_secret_key)
 		auth.set_access_token(access_token, access_token_secret)
@@ -59,7 +78,6 @@ if __name__ == "__main__":
 		argstr = (sys.argv[1] if len(sys.argv) > 1 else "python")
 		stream.filter(track=[argstr])
 	except KeyboardInterrupt:
-		print("Execution has stoped.")
 		try:
 			sys.exit(0)
 		except SystemExit:
